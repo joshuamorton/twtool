0622b8d28f7253d79c1cc54188d1c83a26f021d4
Joshua Morton <joahua.morton13@gmail.com>
2013-08-28 00:10:16 -0400

    testing a change

--- tribalwarstool.py
+++ tribalwarstool.py
@@ -1,8 +1,24 @@
-from bottle import *
-from pages import *
+from bottle import route, run, error
+
+global startCode
+global endCode
+css = "p {color:red;text-align:center;text-size:25}"
+startCode = "<html>\n<head>\n<style type=\"text/css\">\n"+css+"\n</style>\n</head>\n<body>\n"
+endCode = "\n</body>\n</body>"
 
 @route('/')
 def index():
-	return htmlStart+"Hello"+htmlEnd
-	
-run(host="http://twtool.herokuapp.com/", port=int(os.environ.get("PORT", 5000)))
+	return "hello world"
+
+
+@route('/mchammer/<post_id>')
+def cantTouchThis(post_id):
+	return startCode+" <p />You can\'t touch this, even with a " +post_id+"."+endCode
+
+@error(404)
+def notFound(error):
+	return startCode+" <p />This is not the page you are looking for."+endCode
+
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+

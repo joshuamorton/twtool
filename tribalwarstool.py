e0c2944ce9e6843ceb7c79222f19abd47de3e26c
Joshua Morton <joahua.morton13@gmail.com>
2013-08-28 01:10:57 -0400

    updating with html testing

--- tribalwarstool.py
+++ tribalwarstool.py
@@ -1,9 +1,10 @@
 from bottle import route, run, error
+from pages import *
 import os
 
 @route('/')
 def index():
-	return "hello world"
+	return htmlStart+"hello world"+htmlEnd
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
